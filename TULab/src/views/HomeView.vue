f5eb838935447d96469b5c6fe92993d4e81f556a
vencoca <vaclav.kesler@gmail.com>
2022-10-26 17:59:29 +0200

    WIP

--- TULab/src/views/HomeView.vue
+++ TULab/src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">  
+    <section id='Home' class="hero">
+
+    </section>
+    <section id='Technology' class="hero">
+
+    </section>
+    <section id='Calendar' class="hero">
+
+    </section>
+    <section id='Kontakt' class="hero">
+
+    </section>
+  </div>
 </template>
+
+
+<style lang="scss" scoped>
+.hero {
+  position: relative;
+  height: 100vh;
+}
+</style>
