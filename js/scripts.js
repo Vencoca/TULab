817f5a0301d124015740b781f4a7159c06deee1c
vencoca <vaclav.kesler@gmail.com>
2022-11-30 00:13:35 +0100

    1.2

--- js/scripts.js
+++ js/scripts.js
@@ -5599,15 +5599,17 @@ return PhotoSwipeUI_Default;
 
 
 a = document.getElementById("arrow")
-a.addEventListener("click", e => {
-    let element = document.querySelector(a.getAttribute("href"));
-    let linkST = ScrollTrigger.create({
-                    trigger: element,
-                    start: "top 81px"
-                  });
-    e.preventDefault();
-    gsap.to(window, {duration: 1, scrollTo: linkST.start, overwrite: "auto"});
-  });
+if (a !== null) {
+  a.addEventListener("click", e => {
+      let element = document.querySelector(a.getAttribute("href"));
+      let linkST = ScrollTrigger.create({
+                      trigger: element,
+                      start: "top 81px"
+                    });
+      e.preventDefault();
+      gsap.to(window, {duration: 1, scrollTo: linkST.start, overwrite: "auto"});
+    });
+}
   //tween the svg path + circle
   gsap.timeline(
     {scrollTrigger:{
@@ -5627,6 +5629,57 @@ a.addEventListener("click", e => {
 
   //center the container's left/top position
   window.onload = window.onresize = ()=> { gsap.set('#map_container', {left:innerWidth/2, top:innerHeight/2}); }
+function sla_calculate(){
+    const electricity_price = 6;
+    const map_sla_resin = new Map();
+        map_sla_resin.set('Clear', 4.61);
+        map_sla_resin.set('Black', 4.61);
+        map_sla_resin.set('White', 4.61);
+        map_sla_resin.set('Gray', 4.61);
+        map_sla_resin.set('Tough', 5.68);
+        map_sla_resin.set('Durable', 5.68);
+        map_sla_resin.set('High Temp', 5.68);
+        map_sla_resin.set('Grey Pro', 5.68);
+        map_sla_resin.set('ESD', 5.68);
+        map_sla_resin.set('Rigid', 6.40);
+        map_sla_resin.set('Elastic', 6.40);
+        map_sla_resin.set('Flexible', 6.40);
+        map_sla_resin.set('BioMed', 7.99);
+        map_sla_resin.set('Castable', 8.10);
+    const power_consumption = 0.0060;
+    const hourly_rate = 40;
+    const handling_fee = 100;
+    const resin = document.getElementById("sla_resin");
+    const volume = document.getElementById("sla_volume");
+    const time = document.getElementById("sla_time");
+    const output = document.getElementById("sla_price");
+    let time_values = time.value.split(':');
+    let time_hours = parseInt(time_values[0], 10) + parseInt(time_values[1], 10)/60
+    output.textContent = Math.round(map_sla_resin.get(resin.value)*volume.value + time_hours*hourly_rate+handling_fee + time_hours*power_consumption*electricity_price) + " Kč";
+}
+
+function fdm_calculate(){
+    const handling_fee = 50;
+    const weight = document.getElementById("fdm_weight");
+    const weight_price = 6;
+    const hourly_rate = 30;
+    const time = document.getElementById("fdm_time");
+    const output = document.getElementById("fdm_price");
+    let time_values = time.value.split(':');
+    let time_hours = parseInt(time_values[0], 10) + parseInt(time_values[1], 10)/60 
+    output.textContent = Math.round(time_hours*hourly_rate + weight_price*weight.value+handling_fee) + " Kč"
+}
+
+function laser_calculate(){
+    const handling_fee = 50;
+    const material = document.getElementById("laser_material");
+    const hourly_rate = 360;
+    const time = document.getElementById("laser_time");
+    const output = document.getElementById("laser_price");
+    let time_values = time.value.split(':');
+    let time_hours = parseInt(time_values[0], 10) + parseInt(time_values[1], 10)/60 
+    output.textContent = Math.round(time_hours*hourly_rate + parseInt(material.value,10) + handling_fee) + " Kč"
+}
 
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, DrawSVGPlugin);
 
